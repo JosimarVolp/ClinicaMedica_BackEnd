50b3b3699e747d07fe71a2b871c24c65903ffc73
Josimar Volpini <josimarvolpini@pjf.mg.gov.br>
2022-04-03 14:11:30 -0300

    update cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ let bodyParser = require( 'body-parser' );
 
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://clinicamedicapuc.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://clinicamedicapucmg.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
